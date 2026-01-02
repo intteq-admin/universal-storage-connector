6a0d9a3a1c5ab2668be249ac858a7dfd2da43b59
dilhanpathum <dilhan.jayasiri@gmail.com>
2026-01-02 10:04:57 +0530

    skelton project

---
+++ src/main/java/com/intteq/universal_storage_connector/UniversalStorageConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.intteq.universal_storage_connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalStorageConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalStorageConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intteq/universal_storage_connector/UniversalStorageConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intteq.universal_storage_connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalStorageConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
